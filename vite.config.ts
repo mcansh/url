bc48177ad5587a771120beb9d3fe507d12951eab
Logan McAnsh <logan@mcan.sh>
2024-06-10 21:02:59 -0400

    chore: use ts compiler api to get target

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import { loadTsConfig } from "load-tsconfig";
+import ts from "typescript";
 
-const loaded = loadTsConfig(".");
+// load tsconfig file using typescript API
+const tsconfig = "tsconfig.json";
+const loaded = ts.readConfigFile(tsconfig, ts.sys.readFile);
+
+if (loaded.error) {
+  throw new Error(loaded.error.messageText.toString());
+}
+
+if (!loaded.config) {
+  throw new Error("No config found");
+}
 
 export default defineConfig({
   build: {
-    target: loaded.data.compilerOptions.target,
+    target: loaded.config.compilerOptions.target,
     lib: {
       entry: ["src/index.ts"],
       formats: ["es"],
