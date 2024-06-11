a7aab310e96d78a60f75b67b8ad7bcd8c42ff52c
Logan McAnsh <logan@mcan.sh>
2024-06-10 21:08:40 -0400

    chore: use default vite target

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,8 @@
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import ts from "typescript";
-
-// load tsconfig file using typescript API
-const tsconfig = "tsconfig.json";
-const loaded = ts.readConfigFile(tsconfig, ts.sys.readFile);
-
-if (loaded.error) {
-  throw new Error(loaded.error.messageText.toString());
-}
-
-if (!loaded.config) {
-  throw new Error("No config found");
-}
 
 export default defineConfig({
   build: {
-    target: loaded.config.compilerOptions.target,
     lib: {
       entry: ["src/index.ts"],
       formats: ["es"],
