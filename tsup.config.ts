d709619f8f96e1b17832fa52daca8ae3aa90e49f
Logan McAnsh <logan@mcan.sh>
2024-12-13 14:40:34 -0500

    chore: remove extra exports
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: {
-    index: "src/index.ts",
-    builder: "src/lib/builder.ts",
-    url: "src/lib/url.ts",
-  },
+  entry: ["src/index.ts"],
   dts: true,
   format: ["esm"],
   tsconfig: "tsconfig.json",
